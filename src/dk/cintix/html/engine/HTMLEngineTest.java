ef486edda55a25c9860b870107c54fbf9b5577ac
Michael Martinsen <migo@tv2.dk>
2022-01-11 18:00:44 +0100

    updated

--- src/dk/cintix/html/engine/HTMLEngineTest.java
+++ src/dk/cintix/html/engine/HTMLEngineTest.java
@@ -56,7 +56,6 @@ public class HTMLEngineTest {
                     + "</html>";
 
             String result = HTMLEngine.process(file, variables);
-            System.out.println(result);
             if (expResult.equalsIgnoreCase(result)) {
                 System.out.println("\tHTMLEngine.process:: Test pass");
             } else {
