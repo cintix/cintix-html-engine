ca6466a1b8e4ad0f29f5d3a3a714611bd5b640f0
Michael Martisnen <michael@cintix.dk>
2026-05-05 21:28:20 +0200

    build files

---
+++ src/dk/cintix/html/engine/Main.java
@@ -0,0 +1,8 @@
+package dk.cintix.html.engine;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("cintix-html-engine library module. No standalone runtime is configured.");
+    }
+}
